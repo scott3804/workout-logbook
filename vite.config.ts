8fef9545735ec6ec924f4a54b47151269472ecfe
scott3804 <scottmilholland@gmail.com>
2026-05-25 15:44:02 -0700

    Adding tailwind back in

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
